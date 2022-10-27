3f7a92c58c18fc5562d436b449eec4dbf0f5a0e7
Morfishka <salimyanova.tany@mail.ru>
2022-10-28 01:13:01 +0500

    многочлены и запись в файл

---
+++ task_19.py
@@ -0,0 +1,38 @@
+# Задана натуральная степень k. Сформировать случайным образом список коэффициентов 
+# (значения от 0 до 100) многочлена и записать в файл многочлен степени k.
+
+# Пример:
+# - k=2 => 2*x² + 4*x + 5 = 0 или x² + 5 = 0 или 10*x² = 0
+
+from random import randint
+
+data  = open('polynomial.txt','a')
+
+k = int(input("Введите степень k: "))
+coeff = [randint(0,100) for i in range(k + 1)]
+print(coeff)
+
+result = ''
+j = len(coeff)-1
+while j > 0:
+    for i in range(len(coeff)):
+        if len(result) > 0 and coeff[i] > 0:
+            result += ' + '
+        if j == 1:
+            result += str(coeff[i]) + 'x'
+            j -= 1
+        elif coeff[i] == 1:
+            result += 'x^' + str(j)
+            j -= 1
+        elif coeff[i] == 0:
+            continue
+        else:
+            result += str(coeff[i]) + 'x^' + str(j)
+            j -= 1
+    if coeff[-1] != 0:
+        result = result[:len(result) - 3]
+result += ' = 0'
+print(result)
+data.write(f"{result}")
+data.close()
+
