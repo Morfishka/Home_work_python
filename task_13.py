a63ac571ecd2cfca4efcf68085ee18e844ff0395
Morfishka <salimyanova.tany@mail.ru>
2022-11-04 02:21:08 +0500

    использование list comprehension

--- task_13.py
+++ task_13.py
@@ -5,9 +5,16 @@
 # - [1.1, 1.2, 3.1, 5, 10.01] => 0.19
 
 float_list = [2.4, 1.2, 5.8, 14.01]
-numbers = []
-for n in float_list:
-  if n % 1 != 0:
-    numbers.append(round(n % 1,2))
+# numbers = []
+# for n in float_list:
+#   if n % 1 != 0:
+#     numbers.append(round(n % 1,2))
+# result = max(numbers) - min(numbers)
+# print(f'Разница между максимальным и минимальным значениями дробной части = {result}')
+
+
+# улучшение:
+
+numbers = [round(n % 1,2) for n in float_list if n % 1 != 0]
 result = max(numbers) - min(numbers)
-print(f'Разница между максимальным и минимальным значениями дробной части = {result}')
+print(f'Разница между макс. и мин. значениями дробной части = {result}')
