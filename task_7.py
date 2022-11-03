3004389e8fb605a791cc5953cd2787290f029bdd
Morfishka <salimyanova.tany@mail.ru>
2022-11-03 23:40:50 +0500

    использование lambda, list comprehension

--- task_7.py
+++ task_7.py
@@ -2,12 +2,17 @@
 # Пример:
 # - пусть N = 4, тогда [ 1, 2, 6, 24 ] (1, 1*2, 1*2*3, 1*2*3*4)
 
-from math import prod
+from math import factorial, prod
 
 N = int(input("введите число: "))
-numbers = []
-result = []
-for n in range(N):
-    numbers.append(n+1)
-    print(prod(numbers), end =' ')
+# numbers = []
+# for n in range(N):
+#     numbers.append(n+1)
+#     print(prod(numbers), end =' ')
+
+# улучшение:
+
+numbers = [n for n in range(1,N+1)]
+numbers = list(map(lambda N: factorial(N), numbers))
+print(numbers)
 
