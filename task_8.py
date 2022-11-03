53bb24830cb41813e1e6fd0c74b194113c322535
Morfishka <salimyanova.tany@mail.ru>
2022-11-03 23:45:16 +0500

    использование list comprehension

--- task_8.py
+++ task_8.py
@@ -3,8 +3,14 @@
 # - Для n = 6: {1: 4, 2: 7, 3: 10, 4: 13, 5: 16, 6: 19}
 
 n = int(input("Введите число: "))
-numbers = []
-for i in range(1, n+1):
-    numbers.append(round((1+1/i)**i, 2))
+# numbers = []
+# for i in range(1, n+1):
+#     numbers.append(round((1+1/i)**i, 2))
+# print(numbers)
+# print(f" сумма: {sum(numbers)}")
+
+# улучшение:
+
+numbers = [round((1+1/i)**i, 2) for i in range(1, n+1)]
 print(numbers)
 print(f" сумма: {sum(numbers)}")
