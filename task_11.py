eb9d7aaedcfbfd458a8e92515c82ad135b6d0f76
Morfishka <salimyanova.tany@mail.ru>
2022-11-04 01:25:42 +0500

    использование list comprehension

--- task_11.py
+++ task_11.py
@@ -3,13 +3,24 @@
 # Пример:
 # - [2, 3, 5, 9, 3] -> на нечётных позициях элементы 3 и 9, ответ: 12
 
-list = [4, 6, 4, 3, 2, 1, 9, 5]
-i = 1
-sum = 0
-while i < len(list):
-    sum += list[i]
-    i += 2
-print(sum)
+
+from itertools import count
+from os import remove
+
+
+lst = [4, 6, 4, 3, 2, 1, 9, 5]
+# i = 1
+# sum = 0
+# while i < len(lst):
+#     sum += lst[i]
+#     i += 2
+# print(sum)
+
+# улучшение:
+
+r = [lst[i] for i in range(len(lst)) if i % 2 == 1]
+print(sum(r))
+
 
 
         
