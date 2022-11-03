e55c5b6614d9a9c621f7ffa9437ed92f7b67d8d7
Morfishka <salimyanova.tany@mail.ru>
2022-11-04 00:23:57 +0500

    использование list comprehension

--- task_9.py
+++ task_9.py
@@ -5,25 +5,37 @@
 
 from math import prod
 
+# N = int(input("введите число: "))
+
+# array = []
+# array_2 = []
+# new_array = []
+# for i in range(-N,N+1):
+#     array.append(i)
+# print(f'Диапазон элементов : {array}')
+
+# text = open(r'D:\repo\Python_2block\Home_work_python\file.txt')
+
+# for line in text:
+#     array_2.append(int(line))
+# print(f'Позиции элементов: {array_2}')
+
+# for j in array_2:
+#     new_array.append(array[j])
+
+# print(f'Найденные элементы: {new_array}')
+# print(f'Произведение найденных элементов: {prod(new_array)}')
+# text.close()
+
+# улучшение:
+
 N = int(input("введите число: "))
-
-array = []
-array_2 = []
-new_array = []
-for i in range(-N,N+1):
-    array.append(i)
-print(f'Диапазон элементов : {array}')
-
 text = open(r'D:\repo\Python_2block\Home_work_python\file.txt')
-
-for line in text:
-    array_2.append(int(line))
+array = [i for i in range(-N,N+1)]
+print(f'Диапазон элементов : {array}')
+array_2 = [int(line) for line in text]
 print(f'Позиции элементов: {array_2}')
-
-for j in array_2:
-    new_array.append(array[j])
-
+new_array = [array[array_2[i]] for i in range(len(array_2))]
 print(f'Найденные элементы: {new_array}')
 print(f'Произведение найденных элементов: {prod(new_array)}')
-text.close()
-
+text.close() 
